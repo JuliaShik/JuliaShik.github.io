77a1b2cf9661ed3a08478c3539da3b34fe5851d0
Julia Shikunova <Jul.Shik705@yandex.ru>
2020-10-28 19:53:40 +0200

    Initial

---
+++ precache-manifest.c2db0591e6a1d910e8efe8af026a8ae6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c748866129785dd399145d751046fe0e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d74dc6391e31e10c49e6",
+    "url": "/static/css/main.8ec470a6.chunk.css"
+  },
+  {
+    "revision": "56fd66fb1cb74a6b5315",
+    "url": "/static/js/2.180875a9.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.180875a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d74dc6391e31e10c49e6",
+    "url": "/static/js/main.81e02812.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
